952c6584bd799325fdb0ff8dc1d5f6b6ed61016b
Joshua Colvin <joshuacolvin@joshuas-mbp.lan>
2018-10-23 14:48:15 -0400

    Add dynamic validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -6,17 +6,46 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   form: FormGroup;
+  invalid = false;
   phoneRequired = false;
+  defaultPhoneValidators = [Validators.maxLength(10)];
 
-  constructor(fb: FormBuilder) {
-    this.form = fb.group({
-      phone: ['', Validators.maxLength(10)]
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      phone: ['', this.defaultPhoneValidators]
     });
   }
 
   submitForm(value: any) {
-    // handle submit
+    if (this.form.invalid) {
+      this.invalid = true;
+    } else {
+      this.invalid = false;
+    }
+  }
+
+  updateRequiredStatus(event: any) {
+    this.phoneRequired = event;
+
+    if (this.phoneRequired) {
+      this.form
+        .get('phone')
+        .setValidators(
+          Validators.compose([
+            ...this.defaultPhoneValidators,
+            Validators.required
+          ])
+        );
+    } else {
+      this.form
+        .get('phone')
+        .setValidators(Validators.compose(this.defaultPhoneValidators));
+    }
+
+    this.form.get('phone').updateValueAndValidity();
   }
 }
